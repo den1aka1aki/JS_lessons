b9dd7acab9a90c4485626298ca5c9342f2082fa7
den1aka1aki <lavrov.den@gmail.com>
2022-06-02 16:47:34 +0200

    DOM задание 2

--- index2.js
+++ index2.js
@@ -24,34 +24,38 @@ let taskList = document.querySelector('.tasks-list');
 let div1 = document.createElement('div'),
     div2 = document.createElement('div'),
     div3 = document.createElement('div'),
-    form1 = document.createElement('form'),
-    input1 = document.createElement('input'),
-    label1 = document.createElement('label'),
-    span1 = document.createElement('span'),
-    btn1 = document.createElement('button');
+    form = document.createElement('form'),
+    input = document.createElement('input'),
+    label = document.createElement('label'),
+    span = document.createElement('span'),
+    btn = document.createElement('button');
+
+function createBlock(task){
+
+    div1.className = 'task-item';
+    div1.dataset.taskId = task.id;
+    div2.className = 'task-item__main-container';
+    div3.className = 'item__main-content';
+    form.className = 'checkbox-form';
+    input.className = 'checkbox-form__checkbox';
+    input.type = 'checkbox';
+    input.id = 'task-' + task.id;
+    label.htmlFor = 'task-' + task.id;
+    span.className = 'task-item__text';
+    span.innerText = task.text;
+    btn.className = 'task-item__delete-button default-button delete-button';
+    btn.dataset.deleteTaskId = task.id;
+    btn.textContent = 'Удалить';
+
+    taskList.append(div1)
+
+    form.append(input, label);
+    div3.append(form, span);
+    div2.append(div3, btn);
+    div1.append(div2);
+}
+
 
 for (let task of tasks){
-div1.className = 'task-item';
-div1.dataset.taskId = task.id;
-div2.className = 'task-item__main-container';
-div3.className = 'item__main-content';
-form1.className = 'checkbox-form';
-input1.className = 'checkbox-form__checkbox';
-input1.type = 'checkbox';
-input1.id = 'task-' + task.id;
-label1.htmlFor = 'task-' + task.id;
-span1.className = 'task-item__text';
-span1.innerText = task.text;
-btn1.className = 'task-item__delete-button default-button delete-button';
-btn1.dataset.deleteTaskId = task.id;
-btn1.textContent = 'Удалить';
-
-
-taskList.append(div1)
-
-
-div1.append(div2);
-div2.append(div3, btn1);
-div3.append(form1, span1);
-form1.append(input1, label1);
+    createBlock(task);
 }
