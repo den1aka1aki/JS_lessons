c1017a3311fddb5abb75f01a3a6acf9d356cdb43
den1aka1aki <lavrov.den@gmail.com>
2022-06-02 09:56:14 +0200

    DOM задание 1

--- index.js
+++ index.js
@@ -2,45 +2,47 @@
 
 
 
-document.body.innerHTML = '<form class=".create-user-form"> <label>Имя <input type="text" name="userName" placeholder="Введите ваше имя"> </label><labe>Пароль <input type="password" name="password" placeholder="Придумайте Пароль"></labe><button type="submit">Подтвердить</button></form>';
+//document.body.innerHTML = '<form class=".create-user-form"> <label>Имя <input type="text" name="userName" placeholder="Введите ваше имя"> </label><labe>Пароль <input type="password" name="password" placeholder="Придумайте Пароль"></labe><button type="submit">Подтвердить</button></form>';
 
 
 
 
 const newForm = document.createElement('form');
-newForm.className = 'create-user-form';
+const newLabel1 = document.createElement('label')
+const newInput1 = document.createElement('input');
+const newLabel2 = document.createElement('label')
+const newInput2 = document.createElement('input');
+const newButton = document.createElement('button');
 
+
+function setLabelName (label, name,appendTo){
+    label.textContent=name;
+    appendTo.append(label);
+}
+function setInputInfo(input, type, name,plcHolder, appendTo){
+    input.type = type;
+    input.name = name;
+    input.placeholder = plcHolder;
+    appendTo.append(input)
+}
+function setBtn(btnName, type, txtContent, appendTo){
+    btnName.type=type;
+    btnName.textContent = txtContent;
+    appendTo.append(btnName);
+}
+
+
+newForm.className = 'create-user-form';
 document.body.append(newForm);
 
 
+setLabelName(newLabel1,'Имя ',newForm);
+setInputInfo(newInput1, 'text', 'userName', 'Введите ваше имя', newLabel1);
+setLabelName(newLabel2,' Пароль ', newForm);
+setInputInfo(newInput2, 'password', 'password', 'Придумайте Пароль', newLabel2);
+setBtn(newButton, 'submit', 'Подтвердить', newForm);
 
-const newLabel1 = document.createElement('label')
-newLabel1.textContent = 'Имя';
-newForm.append(newLabel1);
 
-const newInput1 = document.createElement('input');
-newInput1.type = 'text';
-newInput1.name = 'userName';
-newInput1.placeholder = 'Введите ваше имя'
-
-newLabel1.append(newInput1);
-
-const newLabel2 = document.createElement('label')
-newLabel2.textContent = 'Пароль';
-newForm.append(newLabel2);
-
-const newInput2 = document.createElement('input');
-newInput2.type = 'password';
-newInput2.name = 'password';
-newInput2.placeholder = 'Придумайте Пароль'
-
-newLabel2.append(newInput2);
-
-const newButton = document.createElement('button');
-newButton.type = 'submit';
-newButton.textContent = 'Подтвердить';
-
-newForm.append(newButton);
 
 
 
