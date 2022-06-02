c98e5c18e12f021628e1bf89589ed539bd754b6b
den1aka1aki <lavrov.den@gmail.com>
2022-06-02 23:19:12 +0200

    DOM задание 2

--- index2.js
+++ index2.js
@@ -20,42 +20,45 @@ const tasks = [
 ];
 
 
-let taskList = document.querySelector('.tasks-list');
-let div1 = document.createElement('div'),
-    div2 = document.createElement('div'),
-    div3 = document.createElement('div'),
-    form = document.createElement('form'),
-    input = document.createElement('input'),
-    label = document.createElement('label'),
-    span = document.createElement('span'),
-    btn = document.createElement('button');
 
-function createBlock(task){
+
+function createTask(parent,task){
+    let div1 = document.createElement('div'),
+        div2 = document.createElement('div'),
+        div3 = document.createElement('div'),
+        form = document.createElement('form'),
+        input = document.createElement('input'),
+        label = document.createElement('label'),
+        span = document.createElement('span'),
+        btn = document.createElement('button');
 
     div1.className = 'task-item';
     div1.dataset.taskId = task.id;
     div2.className = 'task-item__main-container';
-    div3.className = 'item__main-content';
+    div3.className = 'task-item__main-content';
     form.className = 'checkbox-form';
     input.className = 'checkbox-form__checkbox';
     input.type = 'checkbox';
     input.id = 'task-' + task.id;
     label.htmlFor = 'task-' + task.id;
     span.className = 'task-item__text';
-    span.innerText = task.text;
+    span.textContent = task.text;
     btn.className = 'task-item__delete-button default-button delete-button';
     btn.dataset.deleteTaskId = task.id;
     btn.textContent = 'Удалить';
 
-    taskList.append(div1)
+
+    parent.append(div1);
 
     form.append(input, label);
     div3.append(form, span);
     div2.append(div3, btn);
     div1.append(div2);
-}
 
 
-for (let task of tasks){
-    createBlock(task);
 }
+
+let taskList = document.querySelector(".tasks-list");
+for (let task of tasks)
+    createTask(taskList, task);
+
