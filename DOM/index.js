e606196101074db09f241dd356b143b44871047c
den1aka1aki <lavrov.den@gmail.com>
2022-06-09 10:44:28 +0200

    DOM задание 1

--- DOM/index.js
+++ DOM/index.js
@@ -3,7 +3,7 @@
 
 
 document.body.innerHTML = '<form class=".create-user-form"> <label>Имя <input type="text" name="userName" placeholder="Введите ваше имя"> </label><label>Пароль <input type="password" name="password" placeholder="Придумайте Пароль"></label><button type="submit">Подтвердить</button></form>';
-document.body.innerHTML = '<form class=".create-user-form"> <label>Имя <input type="text" name="userName" placeholder="Введите ваше имя"> </label><label>Пароль <input type="password" name="password" placeholder="Придумайте Пароль"></label><button type="submit">Подтвердить</button></form>';
+
 
 
 //
