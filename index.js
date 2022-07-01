8f84126c08151c1e04cec00c00a07c0c876148fd
den1aka1aki <lavrov.den@gmail.com>
2022-07-01 06:05:27 +0200

    "Creating React"

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 
+import './index.css'
+import WB_IMG from './Assets/index.png';
+
 
 console.log("Hello World!");
+
+
+
+const l1_laybel = document.createElement('h1');
+l1_laybel.className = 'capital_row';
+l1_laybel.textContent = 'I love JavaScript'
+document.body.append(l1_laybel);
+
+
+const web_pack_img = document.createElement('img');
+web_pack_img.src = WB_IMG;
+document.body.append(web_pack_img);
+
+l1_laybel.append(web_pack_img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,53 @@
 
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'webpack', 'index.js'),
+    entry: path.resolve(__dirname, 'index.js'),
     output: {
 
-        path: path.resolve(__dirname, 'webpack', 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: ' main.js',
         clean: true,
+    },
+
+    devServer: {
+
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        open: true,
+
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ],
+    module: {
+
+        rules: [
+
+
+
+            {
+
+                test: /\.css$/i,
+
+                use: ['style-loader', 'css-loader'],
+
+            },
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+                type: 'asset/resource',
+
+            },
+
+        ],
+
     },
 };
